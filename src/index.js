44e1766dfb8c97e154a40909af4ea38b6b800ff4
andrew <andrewjthomsen@yahoo.com>
2019-09-18 19:54:43 -0700

    REfactored index.js server file to integrate socket.io library.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const path = require('path')
+const http = require('http');
 const express = require('express');
+const socketio= require('socket.io');
 
-const app= express()
+const app= express();
+// Enables creating a new web server
+const server = http.createServer(app);
+// Configures web sockets to work with server-> function enables us to configure socketio
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+// Fires up http server
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
 })
